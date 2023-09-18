80a6e4be4ae28685953c93b04fc2d309d2e8f655
Yuriy Larin <blandger@gmail.com>
2023-09-18 17:27:29 +0300

    updated

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
 mod lib;
 
+use crate::lib::request::format_json_command;
+use log::debug;
 use std::io::stdin;
 use std::net::TcpStream;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::thread::JoinHandle;
 use std::time::Duration;
-use log::debug;
-use tungstenite::{connect, Message, WebSocket};
 use tungstenite::client::IntoClientRequest;
-use tungstenite::http::{Response as HttpResponse};
+use tungstenite::http::Response as HttpResponse;
 use tungstenite::stream::MaybeTlsStream;
+use tungstenite::{connect, Message, WebSocket};
 use url::Url;
-use crate::lib::request::format_json_command;
 
-fn ws_reader(shared_writer: Arc<Mutex<WebSocket<MaybeTlsStream<TcpStream>>>>) -> Result<Option<Message>, tungstenite::Error>  {
+fn unblocking_read_message(
+    shared_writer: Arc<Mutex<WebSocket<MaybeTlsStream<TcpStream>>>>,
+) -> Result<Option<Message>, tungstenite::Error> {
     match shared_writer.lock().unwrap().read() {
-        Ok(msg) => {
-            Ok(Some(msg))
-        },
+        Ok(msg) => Ok(Some(msg)),
         Err(err) => match err {
             tungstenite::Error::Io(err) => {
                 if err.kind() == std::io::ErrorKind::WouldBlock {
@@ -26,7 +26,7 @@ fn ws_reader(shared_writer: Arc<Mutex<WebSocket<MaybeTlsStream<TcpStream>>>>) ->
                 } else {
                     Err(tungstenite::Error::Utf8)
                 }
-            },
+            }
             _ => Err(tungstenite::Error::ConnectionClosed),
         },
     }
@@ -45,37 +45,43 @@ fn main() {
         Err(err) => {
             eprintln!("err = {:?}", err);
             panic!("exit on error!")
-        },
+        }
     };
+
+    // setting tungstenite's internal tcp-stream into unblocking mode
     match socket.get_mut() {
         tungstenite::stream::MaybeTlsStream::Plain(s) => s.set_nonblocking(true),
         tungstenite::stream::MaybeTlsStream::NativeTls(s) => s.get_mut().set_nonblocking(true),
-        _ => {
-            Ok(())
-        },
-    }.unwrap();
+        _ => Ok(()),
+    }
+    .unwrap();
 
     let shared_socket = Arc::new(Mutex::new(socket));
 
     dump_headers(&response);
-    let unsub_message = format_json_command("UNSUBSCRIBE", "btcusdt", 1);
+    // let unsub_message = format_json_command("UNSUBSCRIBE", "btcusdt", 1);
 
     let shared_reader = shared_socket.clone();
     thread::spawn(move || loop {
-        let mut reader = ws_reader(shared_reader.clone()).unwrap();
+        let mut reader = unblocking_read_message(shared_reader.clone()).unwrap();
         if reader.is_none() {
             continue;
         }
         let msg = reader.take().unwrap();
-        println!("Received: {}", msg);
+        println!("WS Received: {}", msg);
     });
 
     thread::sleep(Duration::from_millis(1000));
+    // connect to several symbols at binance
     for (index, symbol) in symbols_array.iter().enumerate() {
         let sub_message = format_json_command("SUBSCRIBE", symbol, index);
         let shared_writer = shared_socket.clone();
         let thread_handle = thread::spawn(move || -> () {
-            shared_writer.lock().unwrap().send(Message::Text(sub_message)).unwrap();
+            shared_writer
+                .lock()
+                .unwrap()
+                .send(Message::Text(sub_message))
+                .unwrap();
         });
         thread_handle_vec.push(thread_handle);
     }
@@ -90,15 +96,4 @@ fn dump_headers(response: &HttpResponse<Option<Vec<u8>>>) {
     for (ref header, _value) in response.headers() {
         println!("* {}", header);
     }
-}
-
-fn check_finish_app() -> bool {
-    let mut buf = String::new();
-    let _ = stdin().read_line(&mut buf);
-    let control_letter = buf.trim().to_ascii_lowercase();
-    if control_letter == "y" {
-        debug!("entered letter: {control_letter:?}");
-        return true;
-    }
-    false
 }
